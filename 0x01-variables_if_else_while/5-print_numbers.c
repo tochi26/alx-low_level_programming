a4afc2774b7cedc6cbd268d52fe4696d882de5e4
Tochukwu <105463740+tochi26@users.noreply.github.com>
2022-06-03 23:28:52 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,13 @@
  * Return: 0
  */
 int main(void)
-{ 
-char i;
-for (i = '0'; i <= '9'; ++i)
-putchar(i); 
-putchar('\n');
-return (0);
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+		putchar(i);
+
+	putchar('\n');
+
+	return (0);
 }
