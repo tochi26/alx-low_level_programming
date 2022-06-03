bae04926632971dd74dcbc80e8662f20b0a669e0
Tochukwu <105463740+tochi26@users.noreply.github.com>
2022-06-04 00:10:18 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
-/** * main - entry point * * Description: Print all possible different 
- combinations of two digits * Return: 0 */
-int main(void) { int i; int j; int k; for (i = 0; i < 10; ++i) { for (j 
-		= i + 1; j < 10; ++j) {
-			for (k = j + 1; k < 10; ++k) { putchar('0' + i); 
-				putchar('0' + j); putchar('0' + k); if 
-				(i != 7 || j != 8 || k != 9) {
-					putchar(','); putchar(' ');
-				}
+
+/**
+ * main - Print combinations of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 8)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar('\n'); return (0);
+	putchar('\n');
+
+	return (0);
 }
