244160ff86d4f6bb95bed03f849b408fc619a0f1
Tochukwu <105463740+tochi26@users.noreply.github.com>
2022-06-03 23:38:28 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
-/** * main - entry point * * Description: Print numbers 0 to 9 on one 
- line * Return: 0 */
-int main(void) { int i; for (i = 0; i <= 9; ++i) putchar('0' + i); 
-	putchar('\n'); return (0);
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers 0 to 9 on one line
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+		putchar('0' + i);
+
+	putchar('\n');
+
+	return (0);
 }
