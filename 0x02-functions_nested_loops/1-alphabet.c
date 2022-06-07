9e5d40214847af5cf1a442e84730a594f23fb3c7
Tochukwu <105463740+tochi26@users.noreply.github.com>
2022-06-07 15:26:56 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,19 @@
-/* * File: 1-alphabet.c * Auth: Brennan D Baraban */
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
+ */
+
 #include "holberton.h"
-/** * print_alphabet - prints the alphabet in lowercase, followed by a 
- new line. */
-void print_alphabet(void) { char letter; for (letter = 'a'; letter <= 
-	'z'; letter++)
-		_putchar(letter); _putchar('\n');
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }
