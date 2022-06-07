e9e8ea7bea58d3c089f46624f6ac9f6a5b2382b8
tochi26 <you@example.com>
2022-06-07 10:40:28 -0700

    first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,5 @@
-/* * File: 0-holberton.c * Auth: Brennan D Baraban * */
-#include "holberton.h"
-/** * main - Prints "Holberton" followed by a new line. * * Return: 
- Always 0. */
-int main(void) { _putchar('H'); _putchar('o'); _putchar('l'); 
-	_putchar('b'); _putchar('e'); _putchar('r'); _putchar('t'); 
-	_putchar('o'); _putchar('n'); _putchar('\n'); return (0);
+#include <unistd.h>
+/** * _putchar - writes the character c to stdout * @c: The character to print * * Return: On success 1. * On error, -1 is returned, and errno 
+ is set appropriately. */
+int _putchar(char c) { return (write(1, &c, 1));
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+int _putchar(char c); emacs 0-putchar.c
+#include <unistd.h> include "main.h"
+/** * main - Check description * Description: It prints the word _putchar, followed by a new line. * Return: 0. */ int main(void) { char 
+word[8] = "_putchar"; int i; char new_line = '\n'; for (i = 0; i < 8; i++) write(1, &word[i], 1); write(1, &new_line, 1); return (0);
+}
