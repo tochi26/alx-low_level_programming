d45d380ca2c1cc621c4fd7f6d9b0f0ec57770dc5
Tochukwu <105463740+tochi26@users.noreply.github.com>
2022-06-09 15:10:58 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,17 @@
 #include "main.h"
-/** * print_most_numbers - prints number */ void 
-print_most_numbers(void) {
-	int i; for (i = 48; i < 58; i++) { if (i != 50 && i != 52) { 
+
+/**
+ * print_most_numbers - prints number
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+		{
 			_putchar(i);
 		}
 	}
