6881eb96c3a633d8eceaf0d3a4549f7b6386876d
Tochukwu <105463740+tochi26@users.noreply.github.com>
2022-06-09 14:39:38 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,19 @@
 #include "main.h"
-/** * _isupper - checks uppercase * @c: parameter * Return: 0 if sucessful */ int _isupper(int c) { if (c >= 65 
-	&& c < 91) {
+
+/**
+ * _isupper - checks uppercase
+ * @c: parameter
+ * Return: 0 if sucessful
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c < 91)
+	{
 		return (1);
 	}
-	else { return (0);
+	else
+	{
+		return (0);
 	}
 }
