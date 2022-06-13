37186b5bee7222edc3dedb23e411b751a404db05
Tochukwu <105463740+tochi26@users.noreply.github.com>
2022-06-13 16:07:47 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,11 @@
 #include "main.h"
-/** * reset_to_98 - reverts poiter to 98
+
+/**
+ * reset_to_98 - reverts poiter to 98
  * @n: pointer
  */
+
 void reset_to_98(int *n)
-{*n = 98;
+{
+	*n = 98;
 }
